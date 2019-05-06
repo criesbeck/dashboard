1507bed9f4fbb2b2b56259a897ed477c21e1bc9c
Chris Riesbeck <chris.riesbeck@gmail.com>
2019-05-06 17:20:53 -0500

    first real code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const cols = ['Summary', 'Value', 'Velocity', 'Quality', 'Team', 'Client'];
+const data = {
+  "Blue": [[1, 1, 0, -1, 1], [1, 1, 0, 1, 0]],
+  "Green": [[1, 1, 0, 1, 1], [1, 1, 0, 0, 1]],
+  "Orange": [[-1, 1, 0, 0, -1], [0, 1, 0, 0, 0]]
+};
+const dots = {
+  '1': 'green',
+  '0': 'yellow',
+  '-1': 'red'
+};
+
+const summarizeRatings = (ratings) => (
+  Math.sign(ratings.reduce((sum, rating) => sum + rating, 0))
+);
+
+const TeamRating = ({rating}) => (
+  <td><img src={`images/rating-${dots['' + rating]}.png`} alt={rating}/></td>
+)
+
+const TeamRow = ({team, ratings}) => (
+  <tr>
+    <th>{team}</th>
+    <TeamRating rating={summarizeRatings(ratings)} />
+    { ratings.map((rating, index) => <TeamRating key={index} rating={rating} />)}  
+  </tr>
+)
+
+const Dashboard = ({cols, data}) => (
+  <table className="table">
+    <thead>
+      <tr><th>Team</th>{ cols.map(col => <th key={col}>{col}</th>)}</tr>
+    </thead>
+    <tbody>
+      { Object.keys(data).map(team => <TeamRow key={team} team={team} ratings={data[team][0]} />)}
+    </tbody>
+  </table>
+);
+
+
+const App = () => (
+  <div className="App">
+    <h1>Team Dashboard</h1>
+      <Dashboard cols={cols} data={data} />
+  </div>
+);
 
 export default App;
